cb20c1dad045c00adb39e80e741c87465b8720bb
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-05-01 09:09:46 +0200

    adding burger-menu

--- wp-content/themes/pf/header.php
+++ wp-content/themes/pf/header.php
@@ -6,15 +6,74 @@
    <!-- <link rel="stylesheet" href="./resources/css/styles.scss">-->
     <link rel="stylesheet" type="text/css" href="<?= dw_asset('css'); ?>">
     <script src="<?= dw_asset('js') ?>" defer></script>
-    <title><?= wp_title('·', false, 'right') . get_bloginfo('name') ?></title>
+    <title><?= get_the_title() ?> - Portfolio</title>
 </head>
 <body>
 <header class="header">
-    <h1><?= get_bloginfo('name') ?></h1>
-    <p><?= get_bloginfo('description') ?></p>
 
+    <style type="text/css">
+/*        body{
+            font-size: 14px;
+            font-family: "Gotham", sans-serif;
+            line-height: 160%;
+        }
+        h1{
+            font-family: "Gotham", sans-serif;
+            font-size: 40px;
+        }
+
+        h2 .subline{
+            font-size: 32px;
+            font-family: "STIXGeneral", serif;
+            color: var(--black);
+            padding-top: 16px;
+        }
+        h2{
+            font-size: 24px;
+            font-family: "Gotham", sans-serif;
+            color: var(--dark-violet);
+        }
+        a{
+            text-decoration: none;
+            color: var(--dark-violet);
+            font-size: 20px;
+            font-weight: 700;
+        }
+
+        .header{
+            background-color: var(--header);
+        }
+        body{
+  !*          padding-left: 24px;
+            padding-right: 24px;*!
+        }
+
+        img{
+            border-radius: 16px;
+        }*/
+
+    </style>
+
+
+
+    <h1 class="screenreader__only"><?= get_the_title() ?></h1>
+<!--    <figure class="logo">
+        <?php /*= get_the_post_thumbnail(size: 'medium', attr: ['class' => 'logo__img']); */?>
+    </figure>-->
+    <img src="./resources/img/logo.svg" alt="Logo Lisa-Marie Kreins" width="auto" height="24">
     <nav class="nav__header">
         <h2 class="sro">Navigation pricinpale</h2>
+
+        <a href="#content" class="sro">Aller au contenu principal</a>
+
+        <input type="checkbox" id="burger_menu">
+        <label for="burger_menu" class="burger_menu">
+            <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M2 2H22" stroke="#38312F" stroke-width="3" stroke-linecap="round"/>
+                <path d="M2 11H22" stroke="#38312F" stroke-width="3" stroke-linecap="round"/>
+                <path d="M2 20H22" stroke="#38312F" stroke-width="3" stroke-linecap="round"/>
+            </svg>
+        </label>
         <ul class="nav__container__header">
             <?php foreach(dw_get_navigation_links('header') as $link): ?>
                 <li class="nav__item__header nav__item--<?= $link->icon; ?>">
@@ -24,5 +83,5 @@
         </ul>
     </nav>
 </header>
-<main>
+<main id="content">
 
